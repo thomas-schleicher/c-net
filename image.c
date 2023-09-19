2ce9336727d8b08cb69cfbce3fda9d8d1909303e
Ghost_Element <danieldw@edu.aau.at>
2023-09-19 10:21:42 +0200

    img print

--- image.c
+++ image.c
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdio.h>
+
 #include "image.h"
 #include "matrix.h"
 
@@ -8,4 +10,10 @@ typedef struct {
     int image_label;
 } Image;
 
-
+void img_print (Img* img){
+    //print the image
+    matrix_print(img->pixel_values);
+    //print the number of the image
+    printf("%d", img->image_label);
+}
+void img_free (Img* img)
