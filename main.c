9414c43906a42049b3931d02c463a3dd21523da0
Jakob Stornig <jakob.stornig2003@gmail.com>
2023-09-19 18:20:39 +0200

    (demo) demonstration of image loader.

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdio.h>
+#include <malloc.h>
 
 #include "matrix.h"
 #include "image.h"
 
 int main() {
-    Image** images = import_images("../data/train-images.idx3-ubyte", "../data/train-labels.idx1-ubyte", 20);
+    Image** images = import_images("../data/train-images.idx3-ubyte", "../data/train-labels.idx1-ubyte", NULL, 2);
+    img_visualize(images[1]);
 
 }
