13540377ffd32f3dba7478b03fb78e9f484a9c08
Thomas Schleicher <thscleicher@edu.aau.at>
2023-09-19 13:59:54 +0200

    Neural Network.c outline

--- neuronal_network.c
+++ neuronal_network.c
@@ -1,3 +1,16 @@
-//
-// Created by danie on 19.09.2023.
-//
+
+#include "neuronal_network.h"
+
+Neural_Network* new_network(int input_size, int hidden_size, int output_size, double learning_rate);
+void print_network(Neural_Network* network);
+void free_network(Neural_Network* network);
+
+void save_network(Neural_Network* network, char* file);
+Neural_Network* load_network(char* file);
+
+double predict_images(Neural_Network* network, Image** images, int amount);
+Matrix* predict_image(Neural_Network* network, Image*);
+Matrix* predict(Neural_Network* network, Matrix* image_data);
+
+void train_network(Neural_Network* network, Matrix* input, Matrix* output);
+void batch_train_network(Neural_Network* network, Image** images, int size);
